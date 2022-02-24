fa9f7462d23f6fcba0aca04c52336726132dea65
Marsa Emreef <marsa.emreef@gmail.com>
2022-02-24 14:25:38 +0400

    Initial commit project

---
+++ src/App.js
@@ -0,0 +1,6 @@
+export default function App(){
+    return <div>
+        <h1>Hi, I'm Arif Rachim</h1>
+        <p>This website will be up and running over the next several weeks.</p>
+    </div>
+}
