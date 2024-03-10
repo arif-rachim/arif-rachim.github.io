69cf29baaff1b777d469aa3bb72a4ea796485cdd
Arif Rachim <a.arif.r@gmail.com>
2024-03-10 21:49:25 +0400

    Fix bug with docs location

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.rach.im'
+    site: 'https://www.rach.im',
+    build: {
+        // Other build options...
+        assets: 'assets'
+    },
 });
